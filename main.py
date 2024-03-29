7031eaa4c16f0df8b5896185a95759f0f3f92ae7
Viktor <viktor-iskalin@yandex.ru>
2024-03-29 12:40:11 +0300

    add new const

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import pygame
 # инициализация
 pygame.init()
+# константы
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+# создать окно
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+# заголовок окна
+pygame.display.set_caption('Игра Тир')
+# загрузить иконку для игры
+icon = pygame.image.load('')
+
 # для остановки бесконечного цикла
 running = True
 while running:
