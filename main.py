201c202ec2a00443e606f4ff48d105f7e6a52d94
Viktor <viktor-iskalin@yandex.ru>
2024-03-29 15:39:35 +0300

    add all cicle code

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ pygame.display.set_caption('Игра Тир')
 icon = pygame.image.load('img/young-gamer.jpg')
 pygame.display.set_icon(icon)
 # мишень
-target_image = pygame.image.load('img/target.png')
+target_img = pygame.image.load('img/target.png')
 target_width = 80
 target_height = 80
 # координаты мишени с учетом ее размеров
@@ -25,7 +25,28 @@ color = (random.randint(0,255), random.randint(0,255), random.randint(0,255))
 # для остановки бесконечного цикла
 running = True
 while running:
-    pass
+    screen.fill(color)
+    # перебираем все события
+    for event in pygame.event.get():
+        # событие закрыть окно
+        if event.type == pygame.quit:
+            running = False
+        # событие нажатие ЛК мыши
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            # проверка попадания мышки в цель
+            tar_x = target_x < mouse_x < target_x + target_width
+            tar_y = target_y < mouse_y < target_y + target_height
+            if tar_x and tar_y:
+                # снова задать рандомные координаты
+                # координаты мишени с учетом ее размеров
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+    screen.blit(target_img, (target_x, target_y))
+    # обновление экрана
+    pygame.display.update()
+
 
 # завершение и выход из игры
 pygame.quit()
